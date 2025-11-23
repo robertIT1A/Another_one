88b1c707955d5f09490ff052ddaffdd69c310a80
robertIT1A <robertoguevarrajr25@gmail.com>
2025-11-23 20:03:08 +0800

    Add functions act4 and act5 for user input handling

---
+++ data_types.py
@@ -0,0 +1,13 @@
+def act4():
+    name = input("Enter a your full name:")
+
+    print("Your name has", len(name), "characters")
+
+def act5():
+    something = eval(input("Input number:"))
+
+    print("The data type of something is", type(something))
+
+    answer = 23 + something
+
+    print("The answer is ", answer)
